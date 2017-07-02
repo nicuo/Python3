9e3b3f39b7003e1dcedca2be76326b22627bd958
nicuo <nicuo.grape@gmail.com>
2017-07-02 22:10:56 +0900

    renew file

---
+++ Chapter4/4-1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def main():
+    guess_me = 7
+    if guess_me < 7:
+        print("too low")
+    elif guess_me > 7:
+        print("too high")
+    else:
+        print("just right")
+
+if __name__ == '__main__':
+    main()
