8c366fb3134fec8be225f2f301c37dcf82afcac5
nicuo <nicuo.grape@gmail.com>
2017-07-06 23:58:18 +0900

    add new file

---
+++ Chapter4/4-12.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+def main():
+    titles = ['Creature of Habit','Crewel Fate']
+    plots=['A num turns into a monster','A haunted yarn shop']
+    movies = dict(zip(titles,plots))
+    print(movies)
+
+
+if __name__ == '__main__':
+    main()
