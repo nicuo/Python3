f981476824ca5bbbb4c8a5f9e4c827fa6e2e56af
nicuo <nicuo@ubuntu>
2017-07-23 23:34:37 +0900

    add new file

---
+++ Chapter8/study8_10.py
@@ -0,0 +1,6 @@
+import sqlalchemy
+conn = sqlalchemy.create_engine('sqlite:///books.db')
+sql = 'select title from book order by title asc'
+rows = conn.execute(sql)
+for row in rows:
+    print(row)
